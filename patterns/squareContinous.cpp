916ed5fc9be538db5b1e3b844aca5a7dc29352e5
Measum-Shah <shahmeasum008@gmail.com>
2025-03-02 07:11:58 +0500

    completed square pattern

--- patterns/squareContinous.cpp
+++ patterns/squareContinous.cpp
@@ -3,6 +3,22 @@ using namespace std;
 
 int main()
 {
+    int n = 3;
+    int num = 1;
+    for(int i=0 ; i<n ; i++){
+        for(int j =0;j<n;j++){
+           cout << num; 
+           num++;
+           cout<<" ";
+        }
+       cout<<endl;
+    }
+
     
     return 0;
 }
+
+// outer loop = n times
+// see we have 1 2 3 then 4 5 6 then 7 8 9
+// the only thing we understand that each line only prints three or n numberrs
+// so inner loop also runs n times
